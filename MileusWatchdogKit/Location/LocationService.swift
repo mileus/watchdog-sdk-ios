6343d0e01f70bf8b0041acd455b192e7aae84aca
Libor Polehna <polehna.libor@gmail.com>
2021-03-04 10:27:26 +0400

    Added location services.

--- MileusWatchdogKit/Location/LocationService.swift
+++ MileusWatchdogKit/Location/LocationService.swift
@@ -2,18 +2,58 @@
 import Foundation
 import CoreLocation
 
+struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+}
 
 protocol LocationService {
+    
+    typealias UpdateHandler = (Coordinate) -> Void
+    
     var isAllowed: Bool { get }
+    
+    func startUpdating(update: @escaping UpdateHandler)
+    func stopUpdating()
 }
 
 
-final class CoreLocationService: LocationService {
+final class CoreLocationService: NSObject, LocationService {
     
     private let allowedPermissions = Set<CLAuthorizationStatus>([.authorizedAlways, .authorizedWhenInUse])
+    private let locationManager = CLLocationManager()
+    
+    private var updateCallback: UpdateHandler?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
     
     var isAllowed: Bool {
         allowedPermissions.contains(CLLocationManager.authorizationStatus())
     }
     
+    func startUpdating(update: @escaping UpdateHandler) {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdating() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+}
+
+extension CoreLocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        updateCallback?(
+            .init(
+                latitude: location.coordinate.latitude,
+                longitude: location.coordinate.longitude
+            )
+        )
+    }
 }
--- MileusWatchdogKit/Location/MileusWatchdogLocationSync.swift
+++ MileusWatchdogKit/Location/MileusWatchdogLocationSync.swift
@@ -4,7 +4,21 @@ import Foundation
 
 public final class MileusWatchdogLocationSync {
     
+    private let locationService: LocationService = CoreLocationService()
+    
     public init() throws {
+        if !locationService.isAllowed {
+            throw MileusWatchdogError.insufficientLocationPermission
+        }
+    }
+    
+    public func start(completion: (() -> Void)? = nil) {
+        locationService.startUpdating { [weak self] coordinate in
+            self?.handleLocationUpdate(coordinate: coordinate)
+        }
+    }
+    
+    private func handleLocationUpdate(coordinate: Coordinate) {
         
     }
     
